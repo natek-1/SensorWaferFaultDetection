e1627abb1d6e94867d4b40be730fb838ea6befe3
Nathan Gabriel <nategabrielk@icloud.com>
2024-03-22 21:09:26 -0400

    done training pipeline

---
+++ src/pipeline/training_pipeline.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformer
+from src.components.model_trainer import ModelTrainer
+from src.exception import CustomException
+from src.logger import logging
+
+class TrainPipeline:
+
+    def __init__(self):
+        self.data_ingestion = DataIngestion()
+        self.data_transformation = DataTransformer()
+        self.model_trainer = ModelTrainer()
+    
+    def run_pipeline(self):
+        try:
+            data_ingestion = DataIngestion()
+            path = data_ingestion.initial_data_ingestion()
+            data_transformer = DataTransformer()
+            X_train, X_test, y_train, y_test, _ = data_transformer.initiate_data_transformation(path)
+            model_trainer = ModelTrainer()
+            score, path = model_trainer.initiate_model_training(X_train=X_train, X_test=X_test,
+                                                                y_train=y_train, 
+                                                                y_test=y_test)
+            return score
+        except Exception as e:
+            error = CustomException(e, sys)
+            logging.error(error)
+            raise error
--- test.py
+++ test.py
@@ -5,9 +5,7 @@ from src.utils import import_collection_as_dataframe
 from src.constants import *
 import os
 import sys
-from src.components.data_ingestion import DataIngestion
-from src.components.data_transformation import DataTransformer
-from src.components.model_trainer import ModelTrainer
+
 
 #load_dotenv()
 
@@ -20,14 +18,5 @@ from src.components.model_trainer import ModelTrainer
 
 #print(import_collection_as_dataframe(MONGO_DATABASE_NAME, MONGO_COLLECTION_NAME).head())
 
-data_ingestion = DataIngestion()
-path = data_ingestion.initial_data_ingestion()
-data_transformer = DataTransformer()
-X_train, X_test, y_train, y_test, _ = data_transformer.initiate_data_transformation(path)
-model_trainer = ModelTrainer()
-score, path = model_trainer.initiate_model_training(X_train=X_train, X_test=X_test,
-                                                    y_train=y_train, 
-                                                    y_test=y_test)
-print(score)
 
 
